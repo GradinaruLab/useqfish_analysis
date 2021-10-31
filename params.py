35700c8e27254dddebaf4dfb4ec903634d9d8dcd
Min Jee Jang <yksdas@gmail.com>
2021-10-31 07:59:49 -0700

    params (color_shifts) updated

--- params.py
+++ params.py
@@ -5,8 +5,14 @@ cellch = 3      # channel containing cell reference
 sigma = 3       # estimate size of spots to detect
 shift_window_size = 200     # ? TODO
 
-color_shifts = [[8, 0, 0],          # color aberration shifts for each channel to 647 aquired by imaging focalcheck #1, 500nm
-                [-1, 0, 1],
-                [0, 0, 0],
-                [0, 0, 0],
-                [1, -3, 2]]
+# color_shifts = [[8, 0, 0],          # color aberration shifts for each channel to 647 aquired by imaging focalcheck #1, 500nm
+#                 [-1, 0, 1],         # from 210507_focalcheck
+#                 [0, 0, 0],
+#                 [0, 0, 0],
+#                 [1, -3, 2]]
+
+color_shifts = [[ 8.  0.  0.],      # from 211031_focalcheck
+                [-1.  1.  0.],
+                [-1.  1.  0.],
+                [ 0.  0.  0.],
+                [ 1. -5.  2.]]
